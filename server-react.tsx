ef70027dcaabac3d7f3ce98e61ed94bf4f9b7ceb
aldana.michelino <aldana.michelino@gmail.com>
2022-07-11 10:09:02 -0300

    entrega 20

--- server-react.tsx
+++ server-react.tsx
@@ -1,8 +1,8 @@
 // @deno-types="https://deno.land/x/servest@v1.3.1/types/react/index.d.ts"
 import React from "https://dev.jspm.io/react/index.js";
-import {useState, useEffect} from "https://dev.jspm.io/react/index.js";
+import {useState} from "https://dev.jspm.io/react/index.js";
 // @deno-types="https://deno.land/x/servest@v1.3.1/types/react-dom/server/index.d.ts"
-import ReactDOMServer from "https://dev.jspm.io/react-dom/server.js";
+import ReactDOMServer from "https://dev.jspm.io/react-dom@18.2.0/server.js";
 import { createApp } from "https://deno.land/x/servest@v1.3.1/mod.ts";
 
 const app = createApp();
